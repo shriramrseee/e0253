87c30d3a86ea2dcedb1ecad2d5738e12cf35a44c
Shriram Ramesh <shriramrseee@gmail.com>
2020-01-24 10:48:34 +0530

    Update Benches

--- a0/fork_bench.c
+++ a0/fork_bench.c
@@ -5,31 +5,31 @@
 #include <sys/wait.h>
 #include <string.h>
 
-#define RDTSC(U, L) asm ("rdtsc; mov %%edx, %0; mov %%eax, %1;":"=r"(U),"=r"(L)::"%edx", "%eax"); 
+#define RDTSC(U, L) asm ("rdtsc; mov %%edx, %0; mov %%eax, %1;":"=r"(U),"=r"(L)::"%edx", "%eax"); // ASM for reading TSC register
 
 int main() {
     int u1, l1, u2, l2, pid;
     long nc; 
-    char* p = (char *) malloc(MEM);
+    char* p = (char *) malloc(MEM); // Allocating memory
     memset(p, 1, MEM);
 
     for(int i=0; i<ITER; i++) {
 
         RDTSC(u1, l1);
-        pid = fork();
+        pid = fork(); // System call
         RDTSC(u2, l2);
 
-        nc = (((long) (u2-u1)) << 31) + (l2-l1);
+        nc = (((long) (u2-u1)) << 32) | (l2-l1); // Get no. of cycles
 
         if(pid == -1)
             return -1;
         else if(pid == 0) {
-            printf("%d %ld \n", i, nc);
-            exit(0);
+            printf("%d %ld \n", i, nc); // Print from child
+            return 0;
         }        
         else {
-            printf("%d %ld \n", i, nc);
-            wait(NULL);
+            printf("%d %ld \n", i, nc); // Print from parent
+            wait(NULL); // Wait for child
         }
     }
         
--- a0/precious_bench.c
+++ a0/precious_bench.c
@@ -4,21 +4,21 @@
 #include <errno.h>
 #include <string.h>
 
-#define RDTSC(U, L) asm ("rdtsc; mov %%edx, %0; mov %%eax, %1;":"=r"(U),"=r"(L)::"%edx", "%eax");   
+#define RDTSC(U, L) asm ("rdtsc; mov %%edx, %0; mov %%eax, %1;":"=r"(U),"=r"(L)::"%edx", "%eax");  // ASM for reading TSC register
 
 int main() {
     int u1, l1, u2, l2;
     long ret, nc;
-    char* p = (char *) malloc(MEM);
+    char* p = (char *) malloc(MEM); // Allocating memory
     memset(p, 1, MEM);
 
     for(int i=0; i<ITER; i++) {
 
         RDTSC(u1, l1);
-        ret = syscall(335);
+        ret = syscall(335); // System call. 335 is the my_precious call number for x86-64
         RDTSC(u2, l2);
 
-        nc = (((long) (u2-u1)) << 31) + (l2-l1);
+        nc = (((long) (u2-u1)) << 32) | (l2-l1); // Get no. of cycles
 
         printf("%d %ld %ld %d\n", i, ret, nc, errno);
     }
